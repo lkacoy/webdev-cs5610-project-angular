9acac51443cb0a4170310a868ef49e11121e4692
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 19:36:34 -0400

    add a route to profile

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
+import {ProfileComponent} from "./profile/profile.component";
 const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
