46944b1f55da1f6abc995eb94e6b903eaa3dbdc8
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 19:36:07 -0400

    add a route to register

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
 const appRoutes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
