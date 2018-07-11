9c04ae17761f362a8668cbd637e6e22d824e1773
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 19:35:32 -0400

    add a route for login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,4 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
 const appRoutes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent},
 ];
 export const routing = RouterModule.forRoot(appRoutes);
