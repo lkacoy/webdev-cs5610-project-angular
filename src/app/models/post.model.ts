59ac8904d265fccc4e5909c5e46c73ca1a087262
lkacoy <lkacoyannakis@gmail.com>
2018-07-11 22:41:08 -0400

    add post model

---
+++ src/app/models/post.model.ts
@@ -0,0 +1,10 @@
+export class Post {
+  id: number;
+  username: string;
+  postTitle: string;
+  postContent: string;
+  published: boolean;
+  datePublished: string; //must be string and then can format in component.
+  dateCreated: string;
+  topic: string;
+}
