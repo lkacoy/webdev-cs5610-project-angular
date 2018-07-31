de2535ebf94aee2b3f19df204985695e34fb1c59
lkacoy <lkacoyannakis@gmail.com>
2018-07-30 23:23:06 -0400

    update the create post layout

--- src/app/create-post/create-post.component.ts
+++ src/app/create-post/create-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {PostServiceClient} from "../services/post.service.client";
+import {Post} from "../models/post.model";
 
 @Component({
   selector: 'app-create-post',
@@ -8,6 +9,18 @@ import {PostServiceClient} from "../services/post.service.client";
 })
 export class CreatePostComponent implements OnInit {
 
+  post = new Post();
+
+  //list of acceptable topics
+  topicList = [
+    'Science',
+    'Sports',
+    'Poliics',
+    'Nature',
+    'Shopping',
+    'Recipes',
+    'Travel',
+  ];
   constructor(private service:PostServiceClient) { }
 
   ngOnInit() {
@@ -20,12 +33,12 @@ export class CreatePostComponent implements OnInit {
   }
 
   saveDraft() {
-
+    this.post.published = false;
   }
 
   publishPost() {
     if(confirm('Continuing will make your content viewable to others. Continue?')) {
-
+      this.post.published = true;
     }
   }
 
--- src/app/services/post.service.client.ts
+++ src/app/services/post.service.client.ts
@@ -1,3 +1,10 @@
 export class PostServiceClient {
 
+  profile() {
+    return fetch('http://localhost:3000/api/profile',
+      {
+        credentials: 'include', // include, same-origin, *omit
+      })
+      .then(response => response.json());
+  }
 }
