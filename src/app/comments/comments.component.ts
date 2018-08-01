352a1c696c7f1a853a84363bbd4cde6811328ea1
lkacoy <lkacoyannakis@gmail.com>
2018-08-01 17:30:31 -0400

    set post id for the comment

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Comment} from '../models/comment.model';
 import {CommentServiceClient} from "../services/comment.service.client";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-comments',
@@ -11,9 +12,16 @@ export class CommentsComponent implements OnInit {
 
   comment = new Comment();
 
-  constructor(private service:CommentServiceClient) { }
+  constructor(private service:CommentServiceClient,
+              private activatedRoute:ActivatedRoute) {
+    this.activatedRoute.params.subscribe((params) => this.setPostId(params));
+  }
 
   ngOnInit() {
+  }
+
+  setPostId(params) {
+    this.comment.postId = params['id'];
   }
 
   clearComment() {
