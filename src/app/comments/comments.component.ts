63480c7bd4ed8323c91d6840e9365fa3f20a42ea
lkacoy <lkacoyannakis@gmail.com>
2018-07-31 22:36:45 -0400

    clear comment text

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Comment} from '../models/comment.model';
 
 @Component({
   selector: 'app-comments',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
 
+  comment = new Comment();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  clearComment() {
+    this.comment.comment = '';
+  }
+
+  addComment() {
+
   }
 
 }
