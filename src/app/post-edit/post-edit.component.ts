8cbab02bbe67e2a99c038ef4cab0c348279cfeea
lkacoy <lkacoyannakis@gmail.com>
2018-08-02 20:45:13 -0400

    start adding some of the layout and component for editing a post

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PostServiceClient} from "../services/post.service.client";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Post} from "../models/post.model";
 
 @Component({
   selector: 'app-post-edit',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostEditComponent implements OnInit {
 
-  constructor() { }
+  post:Post = new Post();
+  postId;
+  //list of acceptable topics
+  topicList = [
+    'Science',
+    'Sports',
+    'Politics',
+    'Nature',
+    'Shopping',
+    'Recipes',
+    'Travel',
+  ];
+
+  constructor(private service:PostServiceClient,
+              private activatedRoute:ActivatedRoute,
+              private router:Router) {
+    this.activatedRoute.params.subscribe(params => this.setParams(params));
+  }
 
   ngOnInit() {
   }
 
+  setParams(params) {
+    this.postId = params['id'];
+    this.service.findPostById(this.postId)
+      .then(post =>
+        this.post = post
+      );
+  }
+
+  deleteDraft() {
+    if(confirm('Continuing will delete your draft and your post will not be updated. Continue?')) {
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  updatePost() {
+    if(confirm('Are you sure you want to update your post?')) {
+      this.router.navigateByUrl('/viewPost/' + this.postId);
+    }
+  }
 }
