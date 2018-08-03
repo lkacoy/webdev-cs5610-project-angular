5e4b679752151f3c84bffee2fe2d8cebc39b9186
lkacoy <lkacoyannakis@gmail.com>
2018-08-02 22:01:21 -0400

    disable adding comments if there is no user session

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Comment} from '../models/comment.model';
 import {CommentServiceClient} from "../services/comment.service.client";
 import {ActivatedRoute, Router} from "@angular/router";
+import {UserServiceClient} from "../services/user.service.client";
 
 @Component({
   selector: 'app-comments',
@@ -11,10 +12,12 @@ import {ActivatedRoute, Router} from "@angular/router";
 export class CommentsComponent implements OnInit {
 
   comment = new Comment();
+  isLoggedIn = false;
 
   constructor(private service:CommentServiceClient,
               private router:Router,
-              private activatedRoute:ActivatedRoute) {
+              private activatedRoute:ActivatedRoute,
+              private userService:UserServiceClient) {
     this.activatedRoute.params.subscribe((params) => this.setPostId(params));
   }
 
@@ -23,6 +26,12 @@ export class CommentsComponent implements OnInit {
 
   setPostId(params) {
     this.comment.postId = params['id'];
+    this.userService.getCurrentUserSession()
+      .then(user => {
+        if (user.username) {
+          this.isLoggedIn = true;
+        }
+      })
   }
 
   clearComment() {
